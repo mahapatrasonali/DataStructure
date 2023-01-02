9ddc4c10c7e9f28550d53029a8340140fcb96a0d
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-01-02 20:25:46 +0530

    Insertion

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DataStructurePrograms
+{
+    class  Program
+    {
+        public static void Main(string[] args) 
+        {
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.DisplayInsertionSort();
+
+            //BubbleSort bubbleSort = new BubbleSort();
+            //bubbleSort.DisplayBubbleSort();
+           
+            //string str1 = "heater";
+            //string str2 = "reheat";
+
+            //char[] char1 = str1.ToLower().ToCharArray();
+            //char[] char2 = str2.ToLower().ToCharArray();
+
+            //Array.Sort(char1);
+            //Array.Sort(char2);
+
+            //string val1 = new string(char1);
+            //string val2 = new string(char2);
+
+            //Console.WriteLine(val1);
+            //Console.WriteLine(val2);
+
+            //if(val1 ==val2) 
+            //{
+            //    Console.WriteLine("Anagram");
+
+            //}else
+            //{
+            //    Console.WriteLine("Not Anagram");
+            //}
+
+        }
+    }
+}
