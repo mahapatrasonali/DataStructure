659867bc8fd2d10106f82bc79470058bbaeb3b2a
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-01-02 20:14:14 +0530

    Binary Search

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BinarySearchTreeDemo
+{
+    class Program
+    {
+       public static void Main(string[] args) 
+        {
+           BinaryTree binaryTree= new BinaryTree();
+            binaryTree.Add(20);
+            binaryTree.Add(13);
+            binaryTree.Add(9);
+            binaryTree.Add(25);
+            binaryTree.Add(30);
+            binaryTree.Add(22);
+            binaryTree.TransversePreOrder(binaryTree.Root);
+
+
+
+        }
+
+
+    }
+}
